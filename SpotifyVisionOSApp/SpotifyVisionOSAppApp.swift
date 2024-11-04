e43c361d779853a70d371dc2c9358113b40ded65
ArsalanDaud <ArsalanDaud098@gmail.com>
2024-11-04 17:53:01 +0500

    Initial Commit

---
+++ SpotifyVisionOSApp/SpotifyVisionOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyVisionOSAppApp.swift
+//  SpotifyVisionOSApp
+//
+//  Created by ADJ on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyVisionOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
